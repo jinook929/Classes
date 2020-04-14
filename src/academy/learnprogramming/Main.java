6e79aba1c61090123b74c9b0980f9512ed115a9a
jinook929 <jinook929@gmail.com>
2020-04-13 22:03:22 -0400

    add model

--- src/academy/learnprogramming/Main.java
+++ src/academy/learnprogramming/Main.java
@@ -5,10 +5,12 @@ public class Main {
     public static void main(String[] args) {
 
         Car porsche = new Car();
-        Car holden = new Car();
+        Car carrera = new Car();
         porsche.setModel("911");
+        System.out.println(porsche.getModel());
         System.out.println("Model is " + porsche.getModel());
-        holden.setModel("carrera");
-        System.out.println("Model is " + holden.getModel());
+        carrera.setModel("Carrera");
+        System.out.println(carrera.getModel());
+        System.out.println("Model is " + carrera.getModel());
     }
 }

a58fa11375deb17e4d4bc510ef898be9a3fa0bbb
jinook929 <jinook929@gmail.com>
2020-04-14 20:59:07 -0400

    add model

--- src/academy/learnprogramming/Car.java
+++ src/academy/learnprogramming/Car.java
@@ -10,7 +10,7 @@ public class Car {
 
     public void setModel (String model) {
         String validModel = model.toLowerCase();
-        if (validModel.equals("carrera") || validModel.equals(("Commodore"))) {
+        if (validModel.equals("carrera") || validModel.equals(("commodore"))) {
             this.model = model;
         } else {
             this.model = "Unknown";
--- src/academy/learnprogramming/Main.java
+++ src/academy/learnprogramming/Main.java
@@ -6,9 +6,11 @@ public class Main {
 
         Car porsche = new Car();
         Car carrera = new Car();
+
         porsche.setModel("911");
         System.out.println(porsche.getModel());
         System.out.println("Model is " + porsche.getModel());
+        System.out.println(carrera.getModel());
         carrera.setModel("Carrera");
         System.out.println(carrera.getModel());
         System.out.println("Model is " + carrera.getModel());
